d97fdd1add7c636dfe22cabad18998a94be1d21a
Dmitry Bakulin <88339620+evklid009@users.noreply.github.com>
2022-01-20 16:44:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,6 @@ function myMap() {
     marker.setMap(map);
 }
 
-/* app-menu */
-
-
-
 /* slider1 */
 
 $(function(){
@@ -117,7 +113,6 @@ function removeActiveClasses() {
     });
 }
 
-
 /* bubble */
 
 const clip = (v, min, max = Infinity) => {
@@ -163,7 +158,6 @@ bubblestart();
 
 /* cookie */
 
-// функция возвращает cookie с именем name, если есть, если нет, то undefined    
 function getCookie(name) {
     let matches = document.cookie.match(new RegExp(
     "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
@@ -172,20 +166,16 @@ function getCookie(name) {
 }
 let cookiecook = getCookie("cookiecook"),
 cookiewin = document.getElementsByClassName('cookie_notice')[0];    
-// проверяем, есть ли у нас cookie, с которой мы не показываем окно и если нет, запускаем показ
+
 if (cookiecook != "no") {
-    // показываем    
     cookiewin.style.display="block"; 
-    // закрываем по клику
     document.getElementById("cookie_close").addEventListener("click", function(){
         cookiewin.style.display="none";    
-        // записываем cookie на 1 день, с которой мы не показываем окно
         let date = new Date;
         date.setDate(date.getDate() + 1);    
         document.cookie = "cookiecook=no; path=/; expires=" + date.toUTCString();               
     });
 }
-
 
 /* exit */
 
